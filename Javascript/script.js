b2f51774dee077802b06c7684586fa8111125e1e
Maruf Khan <marufatrai12@gmail.com>
2023-11-13 11:16:25 +0600

    JS-Class-6

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+    var firstName  = "Maruf";
+
+    var lastName = " Khan";
+
+    document.write(" My Name is : " + firstName + lastName);
+
+
+
+    document.write("<br/>");
+
+
+
+    var address = "Rajshahi";
+
+    document.write( "I Live In : " +address);
